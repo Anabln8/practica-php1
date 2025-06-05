f03f30473e19bbe79d664ebb855c0e11423d8cea
Anabln8 <anabln26@gmail.com>
2025-06-05 20:47:56 +0200

    archivos back

--- productos-front/src/api.js
+++ productos-front/src/api.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
-const URL_BASE = 'http://api.tudominio.com/api'; // Cambia por tu backend
+const URL_BASE = "http://34.238.117.203/"; // Cambia por tu backend
 
 const api = axios.create({
   baseURL: URL_BASE,
 });
 
 api.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('token');
+  (config) => {
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  error => Promise.reject(error)
+  (error) => Promise.reject(error)
 );
 
 export default api;
